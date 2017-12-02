9a5383e8a097698169fb4c8ec7bbab7ac500f907
Juan García <33761598+garcilanga@users.noreply.github.com>
2017-12-02 11:57:29 +0100

    Update installPython3.6.sh

--- installPython3.6.sh
+++ installPython3.6.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-# Actualizar la lista de paquetes disponibles para Ubuntu
+# Actualiza la base de datos local de paquetes disponibles para Ubuntu
 sudo apt-get update
 
-# Instalar los pre-requisitos
+# Instala los pre-requisitos
 sudo apt-get install build-essential checkinstall -y
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
 
-# Descargar Python desde el sitio web oficial
+# Descarga Python desde el sitio web oficial
 cd /usr/src
 sudo wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz
 
-# Descomprimir el fichero descargado
+# Descomprime el fichero descargado
 sudo tar xzf Python-3.6.3.tgz
 
-# Compilar e instalar Python 3.6.3
+# Compila e instala Python 3.6.3
 cd Python-3.6.3
 sudo ./configure --enable-optimizations
 sudo make altinstall
