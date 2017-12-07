173c27a54a968d05b2278a93dfe9daf3e057e2a1
Juan García <33761598+garcilanga@users.noreply.github.com>
2017-12-07 01:54:08 +0100

    Update installPython3.6_jupyter.sh

--- installPython3.6_jupyter.sh
+++ installPython3.6_jupyter.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+####################################################################################################
+### Añadri Python 3.6 a la lista de lenguajes disponibles en Jupyter Notebook
+####################################################################################################
+
 sudo python3.6 -m pip install ipykernel
 sudo python3.6 -m ipykernel install --user
 
